1f9349fcde759d84398a5d9d06da032dab153b0e
Mangaldeep Das <rockonliku88@gmail.com>
2021-01-04 21:48:53 +0530

    lastOccurance

---
+++ Searching/IndexOFLastOccurance.java
@@ -0,0 +1,38 @@
+package in.mangaldeep;
+
+import java.util.Scanner;
+
+public class IndexOFLastOccurance {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = in.nextInt();
+        }
+        int x = in.nextInt();
+        int l = 0;
+        int h = n;
+        int index = indexOfLastOcc(arr, l, h, x);
+        System.out.println(index);
+
+    }
+
+    private static int indexOfLastOcc(int[] arr, int l, int h, int x) {
+        if(l>h){
+            return -1;
+        }
+        int mid = (l+h)/2;
+        if(arr[mid] == x){
+            if(mid  == arr.length-1 ||arr[mid] != arr[mid+1]){
+                return mid;
+            }else{
+                return indexOfLastOcc(arr,mid+1,h,x);
+            }
+        }else if (arr[mid] > x){
+            return indexOfLastOcc(arr,l,mid-1,x);
+        }else{
+            return indexOfLastOcc(arr, mid+1, h,x);
+        }
+    }
+}
